b39380ba93308a0b9ed7c0ad7f7645701e8eccfa
Tirth1507 <shahtirth197@gmail.com>
2026-07-19 18:06:29 +0530

    Change: Backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://car-dealership-backend-akz1.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
